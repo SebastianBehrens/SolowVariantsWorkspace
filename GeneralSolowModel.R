8a7ec4bf431089bf6369d7b0f836def8ce3e5a76
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-05-14 16:41:50 +0200

    exported the model functions into separate file and sourced them instead

--- GeneralSolowModel.R
+++ GeneralSolowModel.R
@@ -26,13 +26,8 @@ SimulateGeneralSolowModel <- function(paragrid, np, startvals){
     # np for number of periods;
     # vts for vars to simulat
     
-    # Basic Model Functions ---------------------------------
-    GS_MF_KN <- function(s, Y, delta, K){s * Y + (1-delta)*K}
-    GS_MF_LN <- function(n, L){(1+n) * L}
-    GS_MF_AN <- function(g, A){(1+g) * A}
-    GS_MF_RR <- function(A, K, L, alpha){alpha * (K/(A*L))^(alpha - 1)}
-    GS_MF_WR <- function(A, K, L, alpha){A* (1-alpha) * (K/(A*L))^alpha }
-    GS_MF_Y <- function(A, K, L, alpha){K^alpha * (A*L)^(1-alpha)}
+    # Load Basic Model Functions ---------------------------------
+    source("GSModelFunctions.R")
     
     # Initialize Simulation Table ---------------------------------
     sim_table <- create_simulation_table(variable_encoder(meta_GS_variables), np)
