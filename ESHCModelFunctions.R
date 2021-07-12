635060e182eac08ce0a6f964d42ddbb23f94bae6
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-07-12 16:09:34 +0200

    created all functions for human capital version

---
+++ ESHCModelFunctions.R
@@ -0,0 +1,22 @@
+# Extended Solow with Human Capital Model Functions ---------------------------------
+
+ESHC_MF_KN <- function(sK, Y, delta, K){sK * Y + (1-delta)*K}
+ESHC_MF_HN <- function(sH, Y, delta, H){sH * Y + (1-delta)*H}
+ESHC_MF_LN <- function(n, L){(1+n) * L}
+ESHC_MF_AN <- function(g, A){(1+g) * A}
+ESHC_MF_RR <- function(A, H, K, L, alpha, phi){alpha * (K/(A*L))^(alpha - 1)*(H/(A*L))^phi}
+ESHC_MF_WR <- function(A, H, K, L, alpha, phi){(1-alpha) * (K/(A*L))^(alpha)*(H/(A*L))^phi * A}
+ESHC_MF_Y <- function(A, H, K, L, alpha, phi){K^alpha * H^phi * (A*L)^(1-alpha - phi)}
+ESHC_MF_KpEWN <- function(n, g, sK, YpEW, delta, KpEW){(1/((1+n)*(1+g))) * (sK * YpEW + (1-delta)*KpEW)}
+ESHC_MF_HpEWN <- function(n, g, sH, YpEW, delta, KpEW){(1/((1+n)*(1+g))) * (sH * YpEW + (1-delta)*KpEW)}
+
+ESHC_SS_KpEW <- function(sK, sH, alpha, phi, n, g, delta){((sK^(1-phi)*sH^phi)/(n + g + delta + n * g))^(1/(1-alpha - phi))}
+ESHC_SS_HpEW <- function(sK, sH, alpha, phi, n, g, delta){((sK^alpha * sH^(1-alpha))/(n + g + delta + n * g))^(1/(1-alpha- phi))} 
+ESHC_SS_YpEW <- function(sK, sH, n, g, delta, alpha, phi){(((sK^(1-phi)*sH^phi)/(n + g + delta + n * g))^(1/(1-alpha - phi)))^alpha *(((sK^alpha * sH^(1-alpha))/(n + g + delta + n * g))^(1/(1-alpha- phi)))^phi}
+# ESHC_SS_YpEW <- function(HpEW, KpEW, A, alpha, phi){KpEW^alpha * HpEW^phi}
+# ESHC_SS_YpW <- function(HpEW, KpEW, A, alpha, phi){A * KpEW^alpha * HpEW^phi}
+ESHC_SS_YpW <- function(A, sK, sH, n, g, delta, alpha, phi){A * (((sK^(1-phi)*sH^phi)/(n + g + delta + n * g))^(1/(1-alpha - phi)))^alpha *(((sK^alpha * sH^(1-alpha))/(n + g + delta + n * g))^(1/(1-alpha- phi)))^phi}
+
+
+# ESHC_SS_RR <- function(alpha, s, n, g, delta){alpha * (s/(n + g + delta + n * g))^(-1)}
+# ESHC_SS_WR <- function(alpha, s, n, g, delta, A){A * (1- alpha) * (s/(n + g + delta + n * g))^(alpha/(1-alpha))}
