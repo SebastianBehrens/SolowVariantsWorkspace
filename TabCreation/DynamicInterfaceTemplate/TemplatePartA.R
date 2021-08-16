33a30bb9c591068e2c19257e98adbc9362392523
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-08-16 21:44:38 +0200

    dynamically created the dynamic interface (conditional panel for model comparison)

---
+++ TabCreation/DynamicInterfaceTemplate/TemplatePartA.R
@@ -0,0 +1,9 @@
+BSDynamicInterface <- conditionalPanel(condition = "input.ComparingModels_VariantSelection1 == 'BS'",
+                                       # Starting Values ---------------------------------
+                                       titlePanel("Starting Values of Stocks"),
+                                       # StartingValuesCodeAutoFillLineIndexer
+                                       
+                                       titlePanel("Parameter Values"),
+                                       # ParameterCodeAutoFillLineIndexer
+                                       
+)
