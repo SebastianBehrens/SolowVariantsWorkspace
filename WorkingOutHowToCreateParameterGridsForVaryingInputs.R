9aaf683540b7f2b1dde627c72e28e6f377e7bf8f
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-08-16 21:46:12 +0200

    file for working progress (important stuff copied to the proper place in the files)

--- WorkingOutHowToCreateParameterGridsForVaryingInputs.R
+++ WorkingOutHowToCreateParameterGridsForVaryingInputs.R
@@ -53,7 +53,7 @@
 
 
 source("AutomatingShinyAppPageCreation.R")
-create_parameter_grid_advanced <- function(ModelCode, np){
+create_parameter_grid_advanced <- function(ModelCode, np, n_ModelComparison){
     parameternames <- getRequiredParams(ModelCode)
     # iteratively execute calls with the created strings
     parameterchange_valuebefore <- c()
@@ -64,29 +64,45 @@ create_parameter_grid_advanced <- function(ModelCode, np){
     # create strings of parameters
         # i <- "n"
         # ModelCode <- "BS"
-    string_parameterchange_indicator <- paste("ComparingModels_", ModelCode, "_parameterchange_indicator_", partAhelper_1(i), sep = "")
-    string_parameterchange_period <- paste("ComparingModels_", ModelCode, "_parameterchange_period_", partAhelper_1(i), sep = "")
-    string_parameterchange_valuebefore <- paste("ComparingModels_", ModelCode, "_parameterchange_valuebefore_", partAhelper_1(i), sep = "")
-    string_parameterchange_valueafter <- paste("ComparingModels_", ModelCode, "_parameterchange_valueafter_", partAhelper_1(i), sep = "")
+    string_parameterchange_indicator <- paste("ComparingModels",n_ModelComparison, "_", ModelCode, "_parameterchange_indicator_", partAhelper_1(i), sep = "")
+    string_parameterchange_period <- paste("ComparingModels",n_ModelComparison, "_", ModelCode, "_parameterchange_period_", partAhelper_1(i), sep = "")
+    string_parameterchange_valuebefore <- paste("ComparingModels",n_ModelComparison, "_", ModelCode, "_parameterchange_valuebefore_", partAhelper_1(i), sep = "")
+    string_parameterchange_valueafter <- paste("ComparingModels",n_ModelComparison, "_", ModelCode, "_parameterchange_valueafter_", partAhelper_1(i), sep = "")
     
         
         parameterchange_period <- doCall(paste("c(parameterchange_period, if(input$", string_parameterchange_indicator,") input$", string_parameterchange_period," else NA)", sep = ""))
         parameterchange_valuebefore <- doCall(paste("c(string_parameterchange_valuebefore, input$", string_parameterchange_valuebefore, ")", sep = ""))
         parameterchange_valueafter <- doCall(paste("c(string_parameterchange_valueafter, if(input$", string_parameterchange_indicator,") input$", string_parameterchange_valueafter," else NA)", sep = ""))
     }
+    
+    create_parameter_grid(getRequiredParams(ModelCode),
+                          parameterchange_valuebefore,
+                          paraeterchange_period,
+                          paramterchange_valueafter,
+                          np)
 }
-
 
 getRequiredParams <- function(ModelCode){
-    out <- case_when(
-        ModelCode == "BS" ~ c("B", "alpha", "delta", "n", "s"), 
-        ModelCode == "GS" ~ c("g", "alpha", "delta", "n", "s"), 
-        ModelCode == "ESSOE" ~ c("B", "alpha", "n", "s", "r"), 
-        ModelCode == "ESSRL" ~ c("alpha", "beta", "kappa", "delta", "n", "s", "g", "X"), 
-        ModelCode == "ESSRO" ~ c("alpha", "beta", "n", "g", "sE", "s", "delta"), 
-        ModelCode == "ESSROL" ~ c("alpha", "beta", "kappa", "delta", "n", "s", "sE", "g", "X"), 
-        ModelCode == "ESHC" ~ c("alpha", "phi", "n", "g", "sK", "sH", "delta")
-        TRUE ~ NA
-    )
+    out <- if(ModelCode == "BS"){ 
+        out <- c("B", "alpha", "delta", "n", "s")
+        }else if(ModelCode == "GS"){
+        out <- c("g", "alpha", "delta", "n", "s")
+        }else if(ModelCode == "ESSOE"){
+        out <- c("B", "alpha", "n", "s", "r")
+        }else if(ModelCode == "ESSRL"){
+            out <- c("alpha", "beta", "kappa", "delta", "n", "s", "g", "X")
+        }else if(ModelCode == "ESSRO"){
+            out <- c("alpha", "beta", "n", "g", "sE", "s", "delta")
+        }else if(ModelCode == "ESSROL"){
+            out <- c("alpha", "beta", "kappa", "delta", "n", "s", "sE", "g", "X")
+        }else if(ModelCode == "ESHC"){
+            out <- c("alpha", "phi", "n", "g", "sK", "sH", "delta")
+        }else(
+            out <- NaN
+        )
     if(is.na(out)){warning("The entered shortcode for a model variant does not exist.")}
+    return(out)
 }
+
+
+# copied to compare models
