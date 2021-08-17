7ae8611179aa62473195b2661d0a50bada5132f8
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-08-17 11:18:17 +0200

    updated for dynamic variable selection

--- ServerParts/ComparisonServer.R
+++ ServerParts/ComparisonServer.R
@@ -4,28 +4,49 @@
 ComparingModels_Paragrid1 <- reactive({
   create_parameter_grid_advanced(input$ComparingModels_VariantSelection1,
                                  input$ComparingModels1_periods,
-                                 "1", input)
+                                 1, input)
 })
 ComparingModels_Paragrid2 <- reactive({
   create_parameter_grid_advanced(input$ComparingModels_VariantSelection2,
                                  input$ComparingModels1_periods,
-                                 "2", input)
+                                 2, input)
 })
 
-ComparingModel_StartVals1 <- reactive({
+ComparingModels_StartVals1 <- reactive({
     create_startvals_list(input$ComparingModels_VariantSelection1, 1, input)
 })
-ComparingModel_StartVals2 <- reactive({
+ComparingModels_StartVals2 <- reactive({
     create_startvals_list(input$ComparingModels_VariantSelection2, 2, input)
 })
+observe({
+  updateCheckboxGroupInput(session, "ModelComparison_VariableSelection",
+                   label = "",
+                   choices = getVariablesAvailableToBeVisualised(input$ComparingModels_VariantSelection1, input$ComparingModels_VariantSelection2),
+                   selected = getVariablesAvailableToBeVisualised(input$ComparingModels_VariantSelection1,
+                                                                  input$ComparingModels_VariantSelection2)[sample(
+                                                                    c(1:10),3)])
+})
+ModelComparison_VariableSelectionEncoded <- reactive({
+  variable_encoder(input$ModelComparison_VariableSelection)
+})
+# ComparingModels_Visualisation <- reactive({
+#   compareEconomies(
+#     input$ComparingModels_VariantSelection1,
+#     input$ComparingModels_VariantSelection2,
+#     ModelComparison_VariableSelectionEncoded(),
+#     ComparingModels_Paragrid1(),
+#     ComparingModels_Paragrid2(),
+#     list(K = 1, L = 1),  list(K = 1, L = 1, A = 1), input$ComparingModels1_periods, input$ComparingModels2_periods
+#   )
+# })
 ComparingModels_Visualisation <- reactive({
   compareEconomies(
     input$ComparingModels_VariantSelection1,
     input$ComparingModels_VariantSelection2,
-    input$ModelComparison_VariableSelection,
+    ModelComparison_VariableSelectionEncoded(),
     ComparingModels_Paragrid1(),
     ComparingModels_Paragrid2(),
-    ComparingModel_StartVals1(), ComparingModel_StartVals2(), input$ComparingModels1_periods, input$ComparingModels2_periods
+    ComparingModels_StartVals1(), ComparingModels_StartVals2(), input$ComparingModels1_periods, input$ComparingModels2_periods
   )
 })
 output$ComparisonVisualisation <- renderPlot({
